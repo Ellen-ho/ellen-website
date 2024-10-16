7a26641fe4936ef6650381a568884747d0ad62ef
Ellen <ellen31725@gmail.com>
2024-10-17 04:18:11 +0800

    feat: modify header and hero section

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,57 +1,82 @@
+'use client';
+
 import React from 'react';
 import NavLink from './NavLink';
-import { useTranslations } from 'next-intl';
+import ThemeToggle from './ThemeToggle';
 
 interface HeaderProps {
   activeSection: string;
   onLinkClick: (section: string) => void;
+  theme: string;
+  onThemeChange: (newTheme: string) => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ activeSection, onLinkClick }) => {
-  const t = useTranslations('HomePage');
+const Header: React.FC<HeaderProps> = ({
+  activeSection,
+  onLinkClick,
+  theme,
+  onThemeChange,
+}) => {
+  const backgroundColor = theme === 'night' ? '#374150' : '#f0e6d6';
+
   return (
-    <header className="sticky top-0 z-50 flex w-full items-center justify-between p-4">
-      <div className="flex items-center space-x-2">
-        <img
-          src="/images/code-editor-icon.png"
-          alt="Code Editor Icon"
-          className="h-8 w-8"
-        />
-        <div className="text-xl font-bold">Ellen Ho</div>
-      </div>
-
-      <nav className="flex justify-center">
-        <div className="btn-group bg-base-100 rounded-full p-2 shadow-md">
-          <NavLink
-            section="home"
-            label="Home"
-            activeSection={activeSection}
-            onClick={onLinkClick}
+    <header
+      className="fixed left-0 top-0 z-[9999] h-20 w-full"
+      style={{ backgroundColor }}
+    >
+      <div className="mx-auto flex h-full w-full max-w-7xl items-center justify-between px-10 py-4">
+        <div className="flex items-center space-x-8">
+          <img
+            id="logo"
+            src={
+              theme === 'night'
+                ? '/images/code-editor-dark.png'
+                : '/images/code-editor-icon.png'
+            }
+            alt="Code Editor Icon"
+            className="h-8 w-8"
           />
-          <NavLink
-            section="project"
-            label="Project"
-            activeSection={activeSection}
-            onClick={onLinkClick}
-          />
-          <NavLink
-            section="resume"
-            label="Resume"
-            activeSection={activeSection}
-            onClick={onLinkClick}
-          />
-          <NavLink
-            section="contact"
-            label="Contact"
-            activeSection={activeSection}
-            onClick={onLinkClick}
-          />
+          <div
+            className={`text-xl font-bold ${
+              theme === 'night' ? 'text-white' : 'text-black'
+            }`}
+          >
+            Ellen Ho
+          </div>
         </div>
-      </nav>
 
-      <div className="flex items-center space-x-4">
-        <a href="/en">{t('english')}</a>
-        <a href="/zh-TW">{t('chinese')}</a>
+        <nav className="flex justify-center space-x-6">
+          <div className="btn-group rounded-full bg-transparent p-2">
+            <NavLink
+              section="home"
+              label="Home"
+              activeSection={activeSection}
+              onClick={onLinkClick}
+            />
+            <NavLink
+              section="project"
+              label="Project"
+              activeSection={activeSection}
+              onClick={onLinkClick}
+            />
+            <NavLink
+              section="resume"
+              label="Resume"
+              activeSection={activeSection}
+              onClick={onLinkClick}
+            />
+            <NavLink
+              section="contact"
+              label="Contact"
+              activeSection={activeSection}
+              onClick={onLinkClick}
+            />
+          </div>
+        </nav>
+
+        <div className="flex items-center space-x-8">
+          <ThemeToggle theme={theme} onThemeChange={onThemeChange} />
+        </div>
       </div>
     </header>
   );
--- src/component/HeroSection.tsx
+++ src/component/HeroSection.tsx
@@ -1,41 +1,49 @@
 import React from 'react';
-import { useTranslations } from 'next-intl';
 
-const HeroSection = React.forwardRef<HTMLDivElement>((props, ref) => {
-  const t = useTranslations('HomePage');
-  return (
-    <section
-      id="home"
-      ref={ref}
-      className="flex flex-col items-center justify-center px-7 md:h-auto md:px-12  lg:flex lg:flex-row lg:items-start lg:justify-between lg:gap-32 lg:px-36 lg:pt-16"
-    >
-      <div className="mb-8 lg:mb-0 lg:w-1/2">
-        <h1 className="mb-4 text-5xl font-bold"> {t('title')}</h1>
-        <p className="mb-6 text-lg">
-          As a software engineer, I’m passionate about solving complex problems
-          and creating impactful solutions. I push boundaries to build systems
-          that enrich lives and deliver meaningful experiences, always thinking
-          beyond the conventional to bring ideas to life.
-        </p>
-        <div className="mb-6 flex justify-center space-x-4 lg:justify-start">
-          <span className="badge badge-primary">Developer</span>
-          <span className="badge badge-secondary">Physician</span>
-          <span className="badge badge-accent">Writer</span>
-        </div>
-      </div>
+interface HeroSectionProps {
+  theme: string;
+}
 
-      <div className="flex justify-center lg:w-1/2 lg:justify-start">
-        <div className="overflow-hidden rounded-lg">
-          <img
-            src="https://avatar.iran.liara.run/public/79"
-            alt="Ellen Ho"
-            className="h-64 w-64 rounded-full object-cover lg:h-80 lg:w-80"
-          />
+const HeroSection = React.forwardRef<HTMLDivElement, HeroSectionProps>(
+  ({ theme }, ref) => {
+    const textColor = theme === 'night' ? 'text-[#e0e0e0]' : 'text-black';
+
+    return (
+      <section
+        id="home"
+        ref={ref}
+        className="mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-10 md:h-auto lg:flex lg:flex-row lg:items-center lg:justify-between lg:gap-32 lg:pt-16"
+      >
+        <div className="mb-8 lg:mb-0 lg:w-2/3">
+          <h1 className={`mb-4 text-5xl font-bold ${textColor}`}>
+            Hi, I am Ellen Ho 👋
+          </h1>
+          <p className={`mb-6 text-lg ${textColor}`}>
+            As a software engineer, I’m passionate about solving complex
+            problems and creating impactful solutions. I push boundaries to
+            build systems that enrich lives and deliver meaningful experiences,
+            always thinking beyond the conventional to bring ideas to life.
+          </p>
+          <div className="mb-6 flex justify-center space-x-4 lg:justify-start">
+            <span className="badge badge-primary">Developer</span>
+            <span className="badge badge-secondary">Physician</span>
+            <span className="badge badge-accent">Writer</span>
+          </div>
         </div>
-      </div>
-    </section>
-  );
-});
+
+        <div className="flex justify-center lg:w-1/3 lg:justify-start">
+          <div className="overflow-hidden rounded-lg">
+            <img
+              src="https://avatar.iran.liara.run/public/79"
+              alt="Ellen Ho"
+              className="h-80 w-80 rounded-full object-cover lg:h-96 lg:w-96"
+            />
+          </div>
+        </div>
+      </section>
+    );
+  },
+);
 
 HeroSection.displayName = 'HeroSection';
 
