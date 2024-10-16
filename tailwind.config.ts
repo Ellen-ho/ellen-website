12aa1feb8d89fdde12d0cfe6ad0a541d3b67dbe5
Ellen <ellen31725@gmail.com>
2024-10-17 04:23:24 +0800

    feat: modify tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
-import daisyui from "daisyui";
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
 
 const config: Config = {
-  content: ["./src/**/*.{tsx,css}"],
+  content: ['./src/**/*.{tsx,css}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
   daisyui: {
-    themes: ["retro"],
+    themes: ['retro', 'night'],
+    darkTheme: 'night',
   },
   plugins: [daisyui],
 };
+
 export default config;
