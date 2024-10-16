dbf02ef95779ed42b5b45736af7862241b3bf016
Ellen <ellen31725@gmail.com>
2024-10-17 04:22:02 +0800

    feat: modify next config mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 export default withNextIntl(nextConfig);
